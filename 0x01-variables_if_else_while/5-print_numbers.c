2d4b795a12f3d33725a4ef6a7b3e06dbc7d69526
Nabiilahh <nabilasani999@gmail.com>
2022-09-09 10:05:12 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all single digits from o to base 10
+ * Return: Always 0 
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
