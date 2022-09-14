a73831fdc14a764df452f84df5d39da831e6a88f
Nabiilahh <nabilasani999@gmail.com>
2022-09-14 13:37:07 -0700

    task 6

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - Prints putchar
  * Description - prints _putchar
- * Return: 0
+ * Return: Always (0)
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * print_last_digit - prints last digit of a number
  * Description - prints the last digit of a number
  * @n : n is an integer
- * Return : return value of last digit
+ * Return : Always (0)
  */
 int print_last_digit(int n)
 {
