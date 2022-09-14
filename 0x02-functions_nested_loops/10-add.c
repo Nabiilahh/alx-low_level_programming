78b88e3bfea9f874623ac43c5cc3e668f939710b
Nabiilahh <nabilasani999@gmail.com>
2022-09-14 13:13:28 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: first variable
+ * @b: second variable
+ * Description : Adds two numbers
+ * Return: Always (0)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
