5c72d79afedee66cae73cea4d9e7c86975eada77
Nabiilahh <nabilasani999@gmail.com>
2022-09-14 14:09:26 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description : prints if a number is positive or negetive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
