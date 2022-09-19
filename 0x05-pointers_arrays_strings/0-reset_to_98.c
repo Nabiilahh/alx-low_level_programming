b1dfdff486fd66fbcae9515cbd67f26f89044516
Nabiilahh <nabilasani999@gmail.com>
2022-09-19 10:19:27 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - fuction that takes a pointer to an integer parameter
+ * @n: integr pointer
+ * Rerurn: 0 Success
+ */
+void rest_to _98(int *n)
+{
+	*n = 98;
+}
